386f27bd48173b5d1ff34fcf3ef4630639378626
Jouw Naam <jouw@email.com>
2025-04-18 20:50:23 +0200

    Voeg debug-modus en minimale app-versie toe om problemen met laden op te lossen

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
-import App from './App';
+// Import de MinimalApp voor debug-doeleinden
+import MinimalApp from './MinimalApp';
 
-console.log('Index.tsx initialisatie gestart');
+console.log('Index.tsx initialisatie gestart (debug modus)');
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
   throw new Error('Geen "root" element gevonden in de DOM. Controleer je index.html bestand.');
 }
 
-console.log('Root element gevonden, gaat nu app renderen');
+console.log('Root element gevonden, gaat nu debug app renderen');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Gebruik MinimalApp voor diagnose */}
+    <MinimalApp />
   </React.StrictMode>
 ); 
 console.log('Rendering verzonden naar ReactDOM'); 
